97b6fdcd069d157995e0795125360d11ad6de14c
thalamoosie <thalamoose@protonmail.com>
2025-02-06 10:33:21 -0500

    Add UI fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 const SITE_URL = "https://brilliant-cuchufli-a8e8e7.netlify.app/";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
+  integrations: [preact()],
 });
